573e2cec88d9b2fe4df30e4b163bd7668d959f9c
wu-benjamin <benjaminwu24@gmail.com>
2022-08-11 14:35:01 -0400

    use mut in foo test

--- tests/test4_foo.rs
+++ tests/test4_foo.rs
@@ -1,7 +1,7 @@
 fn unsafe_foo(c1: bool, c2: bool, x: i32) -> i32 {
-    let r = if c1 { x + 3 } else { x + 4 };
+    let mut r = if c1 { x + 3 } else { x + 4 };
 
-    let r = if c2 { r - 1 } else { r - 2 };
+    r = if c2 { r - 1 } else { r - 2 };
     assert!(r > x);
     r
 }
@@ -14,9 +14,9 @@ fn safe_foo(c1: bool, c2: bool, mut x: i32) -> i32 {
     if x > i32::MAX - 4 {
         x = i32::MAX - 4;
     }
-    let r = if c1 { x + 3 } else { x + 4 };
+    let mut r = if c1 { x + 3 } else { x + 4 };
 
-    let r = if c2 { r - 1 } else { r - 2 };
+    r = if c2 { r - 1 } else { r - 2 };
     assert!(r > x);
     r
 }

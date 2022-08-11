f05f036d054550a6f8e895b039b0778b39b27b6c
Justin Reiter <JDReiter@uwaterloo.ca>
2022-08-10 23:19:14 -0400

    Implement Symbolic Execution for Integer Functions (#2)
    * add script to gen bc files
    
    * migrate to inkwell
    
    * first attempt parse instruction
    
    * build internal cfg and topological sort
    
    * do not do symbolic exec on main
    
    * draft backward sym exec
    
    * preliminary work on entry conditions
    
    * constrain int inputs
    
    * remove unused assignment
    
    * iterate through instructions in reverse
    
    * program name from end of file path instead of start
    
    * undo botched crate author comment update
    
    
    * Implement icmp, mul and fix store
    
    
    Co-authored-by: wu-benjamin <benjaminwu24@gmail.com>

--- tests/test.rs
+++ tests/test.rs
@@ -1,4 +1,4 @@
-fn foo(c1: bool, c2: bool, x: u64) -> u64 {
+fn unsafe_foo(c1: bool, c2: bool, x: i32) -> i32 {
     let r = if c1 { x + 3 } else { x + 4 };
 
     let r = if c2 { r - 1 } else { r - 2 };
@@ -9,7 +9,19 @@ fn foo(c1: bool, c2: bool, x: u64) -> u64 {
 //     ensures r > x;
 // }
 
+fn safe_foo(c1: bool, c2: bool, mut x: i32) -> i32 {
+    if x > i32::MAX - 4 {
+        x = i32::MAX - 4;
+    }
+    let r = if c1 { x + 3 } else { x + 4 };
+
+    let r = if c2 { r - 1 } else { r - 2 };
+    assert!(r > x);
+    r
+}
+
 fn main() {
     // println!("{}", foo(false, true, 13))
-    foo(false, true, 13);
+    unsafe_foo(false, true, 13);
+    safe_foo(false, true, 13);
 }
---
+++ tests/test_3_old.rs
@@ -0,0 +1,24 @@
+// TODO: implement zext, select, trunc
+fn unsat_safe_1(x: i32) -> () {
+    assert!(x <= -200 || x >= -300);
+}
+
+fn unsat_safe_2(x: i32) -> () {
+    assert!(x == x);
+}
+
+fn sat_unsafe_1(x: i32) -> () {
+    assert!(x < 13);
+}
+
+// TODO: implement zext, select, trunc
+fn sat_unsafe_2(x: i32) -> () {
+    assert!(x <= -200 && x >= -300);
+}
+
+fn main() {
+    unsat_safe_1(12);
+    unsat_safe_2(14);
+    sat_unsafe_1(12);
+    sat_unsafe_2(-250);
+}

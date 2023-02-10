d2f50128377fa2e8a4a3c32e4bd8d3f0591794f6
wu-benjamin <benjaminwu24@gmail.com>
2023-02-10 15:04:18 -0500

    correctly handle unknown sat result cautiously

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,6 +1,5 @@
 use std::fs;
 use std::process::Command;
-use std::env::current_dir;
 
 use tracing_core::Level;
 use tracing_subscriber::FmtSubscriber;
@@ -15,21 +14,15 @@ pub fn test(test_name: &str, function_name: &str, source_code: &str, expected_sa
     // _guard resets the current default dispatcher to the prior default when dropped
     let _guard = tracing::subscriber::set_default(subscriber);
 
-    let source_file_name = format!("tests/ZZZ_temp_test_{}.rs", test_name);
-    let bytecode_file_name = format!("ZZZ_temp_test_{}.bc", test_name);
-    let current_directory_pathbuf = current_dir().unwrap();
-    let current_directory_path = current_directory_pathbuf.as_path();
-    let canonical_current_directory = fs::canonicalize(current_directory_path);
-    let canonical_current_directory_pathbuf = canonical_current_directory.unwrap();
-    let canonical_current_directory_path = canonical_current_directory_pathbuf.as_path();
+    let source_file_name = format!("tests/zzz_temp_test_{}.rs", test_name);
+    let bytecode_file_name = format!("tests/zzz_temp_test_{}.bc", test_name);
 
     // Prevent compiler from optimizing away unused function
     let main = format!("fn main() {{println!(\"{{:p}}\", {} as *const ())}}", function_name);
 
     fs::write(&source_file_name, format!("{}\n{}", source_code, main)).expect("Failed to write temp test file!");
     Command::new("rustc")
-        .args(["--emit=llvm-bc", &source_file_name])
-        .current_dir(canonical_current_directory_path)
+        .args(["--emit=llvm-bc", &source_file_name, "-o", &bytecode_file_name])
         .status()
         .expect("Failed to generate bytecode file!");
 
